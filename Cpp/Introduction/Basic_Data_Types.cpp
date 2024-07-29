abbde807cffac5ebbb03c6daf7414e232b3a0007
Vishal1416 <137073074+Vishal1416@users.noreply.github.com>
2024-07-29 10:44:37 +0530

    Update Basic_Data_Types.cpp
    no need to declare variable("%d%c%ld%lf) in cpp

--- Cpp/Introduction/Basic_Data_Types.cpp
+++ Cpp/Introduction/Basic_Data_Types.cpp
@@ -38,25 +38,24 @@ Output Format
 Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3
 decimal places and the double to 9 decimal places. */
 
-
 #include <iostream>
-#include <cstdio>
+#include <iomanip> // for setprecision
 using namespace std;
 
 int main() {
-    // Complete the code.
     int a;
     long b;
-    long long c;
     char d;
     float e;
-    double f;    
-    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
-    printf("%d\n", a);
-    printf("%ld\n", b);
-    printf("%lld\n", c);
-    printf("%c\n", d);
-    printf("%f\n", e);
-    printf("%lf", f);
+    double f;
+
+    cin >> a >> b >> d >> e >> f;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << d << endl;
+    cout << e << endl; 
+    cout << f << endl; 
+
     return 0;
 }
